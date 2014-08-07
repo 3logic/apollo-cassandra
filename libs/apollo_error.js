697833ce0da80ec58ccb68035fa846eb2bde2015
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2014-08-07 17:58:16 +0200

    improved and shared documentation

--- libs/apollo_error.js
+++ libs/apollo_error.js
@@ -1,5 +1,12 @@
 var util = require('util');
 
+/**
+ * Errors for Apollo.
+ * All errors generated from Apollo which are not directly fired by Cassandra should belong to this list.<br />
+ * Errors have a property `name` which is always {@link ERR_NAME_PREFIX}.{@link AERROR_TYPES}, for example `apollo.model.save.unsetkey`<br />
+ * You can rely on this property to check the error in callback and in try/catch. 
+ * @enum {string} Error code string 
+ */
 var AERROR_TYPES = {
 	'unspecified': {
 		msg: "Unspecified Apollo error ->"
@@ -25,8 +32,15 @@ var AERROR_TYPES = {
 	'model.save.invalidvalue': {
 		msg: "Invalid Value: \"%s\" for Field: %s (Type: %s)"
 	}
-}
+};
 
+/**
+ * Prefix for errors
+ * @readonly
+ * @constant
+ * @default
+ * @type {String}
+ */
 var ERR_NAME_PREFIX = 'apollo';
 
 /**
@@ -48,10 +62,10 @@ var build_error = function(){
 		error_template.msg;
 
 	var error = new Error(error_msg);
-	error.name = ERR_NAME_PREFIX+'.'+name;
+	error.name = ( ERR_NAME_PREFIX ?  ERR_NAME_PREFIX + '.' : '' ) + name;
 	
 	return error;
-}
+};
 
  /**
   * Apollo standard Error object
