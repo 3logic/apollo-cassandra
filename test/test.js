f24aef8ccfe35fd1d5d24f76c0b372dda6ca601f
Niccolo Biondi <niccolo@3logic.it>
2014-08-07 17:38:00 +0200

    test mismatch

--- test/test.js
+++ test/test.js
@@ -85,7 +85,7 @@ describe('Apollo > ', function(){
 
 
         it('add model', function(){
-            var TestModel = ap.add_model("test1", model_test1);
+            var TestModel = ap.get_model("test1", model_test1);
             assert.isFunction(TestModel);
             assert.property(TestModel,'find');
             assert.isFalse(TestModel.is_table_ready());
@@ -94,12 +94,12 @@ describe('Apollo > ', function(){
 
         it('add faulty model (silly type)', function(){
             assert.throws(function(){
-                var TestModel = ap.add_model("test1", faulty_model_test1);
+                var TestModel = ap.get_model("test1", faulty_model_test1);
             });
         });
 
         it('instance model', function(){
-            var TestModel = ap.add_model("test1", model_test1);
+            var TestModel = ap.get_model("test1", model_test1);
             var ins = new TestModel({'v1': 500});
 
             assert.propertyVal(ins,'v1',500);
@@ -110,12 +110,12 @@ describe('Apollo > ', function(){
 
         // describe('Schema conflicts on init',function(){
         //     beforeEach(function(done) {
-        //         var TestModel = ap.add_model("test1", model_test1);
+        //         var TestModel = ap.get_model("test1", model_test1);
         //         TestModel.init(done);
         //     });
 
         //     it('same name, conflicting schema', function(done){
-        //         var TestModel = ap.add_model("test1", model_test2);
+        //         var TestModel = ap.get_model("test1", model_test2);
         //         TestModel.init(function(err,result){
         //             assert.ok(err);
         //             assert.propertyVal(err,'name','apollo.model.tablecreation.schemamismatch');
@@ -124,7 +124,7 @@ describe('Apollo > ', function(){
         //     });
 
         //     it('same name, same schema', function(done){
-        //         var TestModel = ap.add_model("test1", model_test1);
+        //         var TestModel = ap.get_model("test1", model_test1);
         //         TestModel.init(function(err,result){
         //             assert.notOk(err);
         //             done();
@@ -135,15 +135,8 @@ describe('Apollo > ', function(){
         describe('Schema conflicts on init > ',function(){
             
             beforeEach(function(done) {
-                if(ap)
-                    ap.close();
-
-                ap = new Apollo(connection);
-
-                // Setup
-                ap.connect(done);
-
                 var BaseModel = ap.get_model("test1", model_test1);
+                BaseModel.init(done);
             });
 
             var conflict_model = model_test3;
@@ -185,7 +178,7 @@ describe('Apollo > ', function(){
             });
 
             it('same name, same schema', function(done){
-                var TestModel = ap.add_model("test1", model_test1);
+                var TestModel = ap.get_model("test1", model_test1);
                 TestModel.init(function(err,result){
                     assert.notOk(err);
                     done();
@@ -200,7 +193,7 @@ describe('Apollo > ', function(){
             var TestModel;
 
             beforeEach(function(done) {
-                TestModel = ap.add_model("test1", model_test1);
+                TestModel = ap.get_model("test1", model_test1);
                 TestModel.init(done);
             });
 
@@ -234,7 +227,7 @@ describe('Apollo > ', function(){
 
 
         it.skip('pig update', function(done){
-            ap.add_model("test1", model_test1, true, function(err,data){
+            ap.get_model("test1", model_test1, true, function(err,data){
                 ap.pig_cql_update_connection("test1",true, function(err,data){
                     if(err) 
                         console.log('err: '+err);
