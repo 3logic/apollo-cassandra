75ebbd00189f56252e3197ce412d27636e60ac65
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2014-10-08 16:40:49 +0200

    Added types

--- libs/cassandra_types.js
+++ libs/cassandra_types.js
@@ -35,19 +35,35 @@ TYPE_MAP.validators.is_uuid = function (obj){
     return obj.test(pattern_uuid1);
 };
 
+TYPE_MAP.validators.is_inet = function (obj){
+    if(!check.string(obj))
+        return false;
+
+    //var pattern_uuid4 = /^[0-9A-F]{8}-[0-9A-F]{4}-4[0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-9A-F]{12}$/i;
+    var patt_ip4 = /^(25[0-5]|2[0-4]\d|[0-1]?\d?\d)(\.(25[0-5]|2[0-4]\d|[0-1]?\d?\d)){3}$/i,
+        patt_ip6_1 = /^(?:[0-9a-fA-F]{1,4}:){7}[0-9a-fA-F]{1,4}$/i,
+        patt_ip6_2 = /^((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)::((?:[0-9A-Fa-f]{1,4}(?::[0-9A-Fa-f]{1,4})*)?)$/i;
+    return obj.test(patt_ip4) || obj.test(patt_ip6_1) || obj.test(patt_ip6_2) ;
+};
+
 
 TYPE_MAP = {    
+    "ascii"     : {validator : TYPE_MAP.validators.is_string,   dbvalidator : "org.apache.cassandra.db.marshal.AsciiType"},    
     "bigint"    : {validator : TYPE_MAP.validators.is_integer,  dbvalidator : "org.apache.cassandra.db.marshal.LongType"},
     "blob"      : {validator : TYPE_MAP.validators.is_anything, dbvalidator : "org.apache.cassandra.db.marshal.BytesType"},
     "boolean"   : {validator : TYPE_MAP.validators.is_boolean,  dbvalidator : "org.apache.cassandra.db.marshal.BooleanType"},        
+    "counter"   : {validator : TYPE_MAP.validators.is_integer,  dbvalidator : "org.apache.cassandra.db.marshal.CounterColumnType"},
     "decimal"   : {validator : TYPE_MAP.validators.is_number,   dbvalidator : "org.apache.cassandra.db.marshal.DecimalType"},        
     "double"    : {validator : TYPE_MAP.validators.is_number,   dbvalidator : "org.apache.cassandra.db.marshal.DoubleType"},
     "float"     : {validator : TYPE_MAP.validators.is_number,   dbvalidator : "org.apache.cassandra.db.marshal.FloatType"},
+    "inet"      : {validator : TYPE_MAP.validators.is_inet,     dbvalidator : "org.apache.cassandra.db.marshal.InetAddressType"},
     "int"       : {validator : TYPE_MAP.validators.is_integer,  dbvalidator : "org.apache.cassandra.db.marshal.Int32Type"},
     "text"      : {validator : TYPE_MAP.validators.is_string,   dbvalidator : "org.apache.cassandra.db.marshal.UTF8Type"},
-    "timestamp" : {validator : TYPE_MAP.validators.is_datetime, dbvalidator : "org.apache.cassandra.db.marshal.TimestampType"},        
-    "varint"    : {validator : TYPE_MAP.validators.is_integer,  dbvalidator : "org.apache.cassandra.db.marshal.IntegerType"},
-    "uuid"      : {validator : TYPE_MAP.validators.is_uuid,     dbvalidator : "org.apache.cassandra.db.marshal.UUIDType"}
+    "timestamp" : {validator : TYPE_MAP.validators.is_datetime, dbvalidator : "org.apache.cassandra.db.marshal.TimestampType"},
+    "timeuuid"  : {validator : TYPE_MAP.validators.is_uuid,     dbvalidator : "org.apache.cassandra.db.marshal.TimeUUIDType"},
+    "uuid"      : {validator : TYPE_MAP.validators.is_uuid,     dbvalidator : "org.apache.cassandra.db.marshal.UUIDType"},
+    "varchar"   : {validator : TYPE_MAP.validators.is_string,   dbvalidator : "org.apache.cassandra.db.marshal.UTF8Type"},    
+    "varint"    : {validator : TYPE_MAP.validators.is_integer,  dbvalidator : "org.apache.cassandra.db.marshal.IntegerType"}
 };
 
 TYPE_MAP.find_type_by_dbvalidator = function(val){
